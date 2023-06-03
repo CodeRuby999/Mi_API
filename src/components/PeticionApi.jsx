84705c4e9550e10950842b0a2680874c82758db2
Luis Cavadia <luisfer.2004@hotmail.com>
2023-06-03 11:10:01 -0500

    Fifth Commit

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -12,3 +12,11 @@ const PeticionApi = () => {
         const res = await fetch(`https://swapi.dev/api/people/?page=${paginacion}`);
         const respuesta = await res.json();
         const auxPersonajes = respuesta.results;  
+        
+// Obtener las imágenes de los personajes desde la API Star Wars Images
+        const personajesConImagenes = await Promise.all(auxPersonajes.map(async (personaje) => {
+            try {
+            const res = await axios.get(`https://starwars-visualguide.com/assets/img/characters/${getPersonajeId(personaje.url)}.jpg`);
+            personaje.image = res.request.responseURL;
+            } catch (error) {
+            console.log(`Error al obtener la imagen del personaje ${personaje.name}: ${error.message}`);
